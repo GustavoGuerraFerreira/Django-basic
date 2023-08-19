8e40930a3af6e38d5fa3e6f568de9642d0a3afa0
Gustavo <gutvasco123@gmail.com>
2023-08-19 14:11:18 -0300

    trazendo os dados da view para o template

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-        return render(request, 'index.html')
+        nome = 'Gustavo'
+        idade = 20
+        profissao = 'Engenheiro'
+        dic ={'nome': nome, 'idade' : idade, "profissao" : profissao}
+        return render(request, 'cadastro/index.html', {'nome': nome, 'idade' : idade, "profissao" : profissao})
