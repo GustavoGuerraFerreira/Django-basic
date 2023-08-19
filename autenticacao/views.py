5882661cc4014afbafc3b1c4c824ba120a8fb0ef
Gustavo <gutvasco123@gmail.com>
2023-08-19 14:27:43 -0300

    manipulando lista de dicionários

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-        nome = 'Gustavo'
-        idade = 20
-        profissao = 'Engenheiro'
-        dic ={'nome': nome, 'idade' : idade, "profissao" : profissao}
-        return render(request, 'cadastro/index.html', {'nome': nome, 'idade' : idade, "profissao" : profissao})
+        pessoa = [{'nome':  'Gustavo', 'idade' : 20, "profissao" : 'Engenheiro'},
+                  {'nome':  'João', 'idade' : 22, "profissao" : 'Médico'}]
+       
+        return render(request, 'cadastro/index.html', {'pessoa': pessoa})
