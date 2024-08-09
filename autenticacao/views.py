c26e98f55cc37b551a16ca328cef461daef5084a
GustavoGuerraFerreira <gutgudino@hotmail.com>
2024-08-09 16:45:01 -0300

    trazendo arquivos estáticos

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-        pessoa = [{'nome':  'Gustavo', 'idade' : 20, "profissao" : 'Engenheiro'},
-                  {'nome':  'João', 'idade' : 22, "profissao" : 'Médico'}]
        
-        return render(request, 'cadastro/index.html', {'pessoas': pessoa, "x" :0})
+        return render(request, 'cadastro/index.html')
