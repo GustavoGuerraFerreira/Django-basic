420deeb23b10c1226f5f7775232ac6cda2e0a062
GustavoGuerraFerreira <gutgudino@hotmail.com>
2024-08-13 17:25:15 -0300

    requisição get

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-       
-        return render(request, 'cadastro/index.html')
+    print(request.GET.get('nome'))
+    nome = request.GET.get('nome')
+    sobrenome = request.GET.get('sobrenome')
+    idade = request.GET.get('idade')
+
+
+    return render(request, 'cadastro/index.html', {'nome': nome, 'sobrenome': sobrenome, 'idade': idade})
